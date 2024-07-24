985e5f51abf0ac354c5152f600edeaa77084c1b2
plengg <81363675+creative-kong@users.noreply.github.com>
2024-07-24 12:35:04 +0700

    update

---
+++ movie/Controllers/UploadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace movie.Controllers
+{
+    public class UploadController : Controller
+    {
+        [HttpPost]
+        public IActionResult Upload (IFormFile file)
+        {
+            return Ok();
+        }
+    }
+}
--- movie/wwwroot/js/config.js
+++ movie/wwwroot/js/config.js
@@ -3,6 +3,7 @@ const show_modal_banner = document.getElementById('show_modal_banner')
 const close_banner_modal = document.getElementById('close_banner_modal')
 const dropzone = document.getElementById('dropzone')
 const file_upload = document.getElementById('file_upload')
+const banner_submit = document.getElementById('banner_submit')
 
 add_banner.addEventListener('click', function () {
     show_modal_banner.classList.remove('hidden')
@@ -14,26 +15,11 @@ close_banner_modal.addEventListener('click', function () {
     show_modal_banner.classList.remove('opacity-1')
 })
 
-dropzone.addEventListener('dragover', e => {
-    e.preventDefault();
-    dropzone.classList.add('border-indigo-600')
-})
-
-dropzone.addEventListener('dragleave', e => {
-    e.preventDefault();
-    dropzone.classList.remove('border-indigo-600')
-})
-
-dropzone.addEventListener('drop', e => {
-    e.preventDefault();
-    dropzone.classList.remove('border-indigo-600')
-    var file = e.dataTransfer.files[0]
-    displayPreview(file)
-});
-
-file_upload.addEventListener('change', e => {
+file_upload.addEventListener('change', function (e) {
+    console.log('change')
     var file = e.target.files[0]
     displayPreview(file)
+    uploadFile(file).then().catch(err => console.log(err))
 });
 
 function displayPreview(file) {
@@ -45,3 +31,28 @@ function displayPreview(file) {
         preview.classList.remove('hidden')
     };
 }
+
+banner_submit.addEventListener('click', function (e) {
+    e.preventDefault()
+    let file = file_upload.files[0]
+    if (file) {
+        console.log(file)
+        uploadFile(file)
+    }
+})
+
+async function uploadFile(file) {
+    console.log("update")
+    const formFile = new FormData()
+    const request = new Request('/upload')
+    formFile.append('file', file)
+    try {
+        let response = await fetch(request, {
+            method: 'POST',
+            body: formFile
+        })
+        console.log(response)
+    } catch (err) {
+        console.log(err)
+    }
+} 
